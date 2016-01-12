20fcdc83bea30eaadcc99d8592f24a052abc6982
Echo <s201407073@emails.bjut.edu.cn>
2016-01-12 21:29:31 +0800

    add ICrawler, BaiduCrawler, GoogleCrawler realize Search, GetList, GetBibTex method.
    add BaiduCrawler, GoogleCrawler UnitTest
    realize TestGetBibTex, TestSearch method.

---
+++ BibCrawler/ICrawler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibCrawler
+{
+    public interface ICrawler
+    {
+        /// <summary>
+        /// Search by keyword.
+        /// </summary>
+        /// <param name="keyword"></param>
+        void Search(string keyword);
+
+        /// <summary>
+        /// Get search result list.
+        /// </summary>
+        /// <returns></returns>
+        IList<BriefEntry> GetList();
+
+        /// <summary>
+        /// Get BibTex by search result item.
+        /// </summary>
+        /// <param name="briefEntry"></param>
+        /// <returns></returns>
+        string GetBibTex(BriefEntry briefEntry);
+    }
+}
---
+++ BibCrawlerUnitTest/GoogleCrawlerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BibCrawler;
+
+namespace BibCrawlerUnitTest
+{
+    [TestClass]
+    public class GoogleCrawlerTest
+    {
+        [TestMethod]
+        public void TestSearch()
+        {
+            GoogleCrawler googleCrawler = new GoogleCrawler();
+            googleCrawler.Search("hadoop");
+        }
+
+        [TestMethod]
+        public void TestGetBibTex()
+        {
+            GoogleCrawler googleCrawler = new GoogleCrawler();
+            googleCrawler.Search("搜索");
+            var list = googleCrawler.GetList();
+            var bibTex = googleCrawler.GetBibTex(list[0]);
+        }
+    }
+}
