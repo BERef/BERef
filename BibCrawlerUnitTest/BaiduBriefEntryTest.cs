da47546fb9155f13d1999f753f72604624101b2e
Echo <s201407073@emails.bjut.edu.cn>
2016-01-13 20:20:33 +0800

    Add BaiduBriefEntryTest, GoogleBriefEntryTest

---
+++ BibCrawlerUnitTest/BaiduBriefEntryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BibCrawlerUnitTest
+{
+    [TestClass]
+    public class BaiduBriefEntryTest
+    {
+        [TestMethod]
+        public void TestGetBibTex()
+        {
+        }
+    }
+}
