dc650e7e24b1c0fc54fc4247c6c7dd4e09adf7da
Echo <s201407073@emails.bjut.edu.cn>
2016-01-13 20:20:08 +0800

    Add BaiduBriefEntry, GoogleBriefEntry
    This two classes inherit BriefEntry, and realize GetBibTex().

---
+++ BibCrawler/BaiduBriefEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibCrawler
+{
+    class BaiduBriefEntry : BriefEntry
+    {
+        public override string GetBibTex()
+        {
+            var webRequest = WebRequest.Create(CiteUrl);
+            var webResponse = webRequest.GetResponse();
+
+            string bibtex = null;
+            using (var reader = new StreamReader(webResponse.GetResponseStream()))
+            {
+                bibtex = reader.ReadToEnd();
+            }
+            return bibtex;
+        }
+    }
+}
---
+++ BibCrawler/GoogleBriefEntry.cs
@@ -0,0 +1,32 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibCrawler
+{
+    class GoogleBriefEntry : BriefEntry
+    {
+        private string _googleScholarURL = "https://scholar.google.com";
+        public override string GetBibTex()
+        {
+            var citeWeb = new HtmlWeb();
+            // Get Bibtex url from cite url list.
+            var citeUrl = $"{_googleScholarURL}{WebUtility.HtmlDecode(citeWeb.Load(CiteUrl).DocumentNode.SelectSingleNode("//a[@class='gs_citi']")?.Attributes["href"].Value)}";
+
+            var webRequest = WebRequest.Create(citeUrl);
+            var webResponse = webRequest.GetResponse();
+
+            string bibtex = null;
+            using (var reader = new StreamReader(webResponse.GetResponseStream(), Encoding.Default))
+            {
+                bibtex = reader.ReadToEnd();
+            }
+            return bibtex;
+        }
+    }
+}
