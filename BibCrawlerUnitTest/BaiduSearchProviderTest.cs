202ec62d9e13df5e6d7cf40e4db93c916a7e736e
Blueve <672454911@qq.com>
2016-01-13 14:13:03 +0800

    Update test case

---
+++ BibCrawlerUnitTest/BaiduSearchProviderTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BibCrawlerUnitTest
+{
+    [TestClass]
+    public class BaiduSearchProviderTest
+    {
+        [TestMethod]
+        public void TestGetResult()
+        {
+            BibCrawler.BaiduSearchProvider baiduCrawler = new BibCrawler.BaiduSearchProvider("Search");
+            baiduCrawler.GetResult();
+        }
+
+        [TestMethod]
+        public void TestGetBibTex()
+        {
+            //BibCrawler.BaiduCrawler baiduCrawler = new BibCrawler.BaiduCrawler();
+            //baiduCrawler.Search("搜索");
+            //var list = baiduCrawler.GetResult();
+            //var bibTex = baiduCrawler.GetBibTex(list[0]);
+        }
+    }
+}
---
+++ BibCrawlerUnitTest/GoogleSearchProviderTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BibCrawler;
+
+namespace BibCrawlerUnitTest
+{
+    [TestClass]
+    public class GoogleSearchProviderTest
+    {
+        [TestMethod]
+        public void TestGetResult()
+        {
+            GoogleSearchProvider googleCrawler = new GoogleSearchProvider("Hadoop");
+            var result = googleCrawler.GetResult();
+        }
+
+        [TestMethod]
+        public void TestGetBibTex()
+        {
+            GoogleSearchProvider googleCrawler = new GoogleSearchProvider("Hadoop");
+            var result = googleCrawler.GetResult();
+            var bibTex = googleCrawler.GetBibTex(result[0]);
+        }
+    }
+}
