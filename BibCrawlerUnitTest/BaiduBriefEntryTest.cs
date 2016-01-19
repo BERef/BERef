6871558417b5d6e8383c4592880c09bc5b73ae6a
Echo <s201407073@emails.bjut.edu.cn>
2016-01-19 20:56:40 +0800

    Add unit test TestGetBibTex()

--- BibCrawlerUnitTest/BaiduBriefEntryTest.cs
+++ BibCrawlerUnitTest/BaiduBriefEntryTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BibCrawler;
 
 namespace BibCrawlerUnitTest
 {
@@ -9,6 +10,10 @@ namespace BibCrawlerUnitTest
         [TestMethod]
         public void TestGetBibTex()
         {
+            var entry = new BaiduBriefEntry();
+            // keyword is "hadoop"
+            entry.CiteUrl = "http://xueshu.baidu.com/u/citation?url=http%3A%2F%2Fdl.acm.org%2Fcitation.cfm%3Fid%3D2285539&sign=190db10476f7878b15624a9c076ef027&t=bib";
+            var bib = entry.BibTeX;
         }
     }
 }
--- BibCrawlerUnitTest/GoogleBriefEntryTest.cs
+++ BibCrawlerUnitTest/GoogleBriefEntryTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using BibCrawler;
 namespace BibCrawlerUnitTest
 {
     [TestClass]
@@ -9,6 +9,10 @@ namespace BibCrawlerUnitTest
         [TestMethod]
         public void TestGetBibTex()
         {
+            var entry = new GoogleBriefEntry();
+            // keyword is "hadoop"
+            entry.CiteUrl = "https://scholar.google.com/scholar.bib?q=info:rLXN4dYUFrAJ:scholar.google.com/&output=cite&scirp=0&hl=zh-CN";
+            var bib = entry.BibTeX;
         }
     }
 }
