763fbd10c6416731a54d7af10912c06179b7b13d
EchoYang33 <echo_register@163.com>
2016-01-21 14:15:10 +0800

    Initial project

--- ScholarProviderInterface/IBuilder.cs
+++ ScholarProviderInterface/IBuilder.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace ScholarProviderInterface
 {
-    public class Class1
+    public interface IBuilder<T>
     {
+        BriefEntry Build(T item);
+
     }
 }
