67ee340489cd971d736a8493e3a10fc7493ab36a
Blueve <672454911@qq.com>
2016-01-22 14:32:51 +0800

    Add RuleSet class

---
+++ GoogleScholarProvider/RuleSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleScholarProvider
+{
+    public static class RuleSet
+    {
+        #region Public Static Field
+        public static string GoogleScholarURL = "https://scholar.google.com";
+        public static string CiteUrlPath = "//a[@class='gs_citi']";
+        #endregion
+    }
+}
