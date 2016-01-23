d17de5471cfd7b83e4fb8e915ca87a322f16ec18
Blueve <672454911@qq.com>
2016-01-24 00:36:05 +0800

    Update

--- GoogleScholarProvider/RuleSet.cs
+++ GoogleScholarProvider/RuleSet.cs
@@ -9,8 +9,17 @@ namespace GoogleScholarProvider
     public static class RuleSet
     {
         #region Public Static Field
-        public static string GoogleScholarURL = "https://scholar.google.com";
-        public static string CiteUrlPath = "//a[@class='gs_citi']";
+        public static string GoogleScholarURL       = "https://scholar.google.com";
+        public static string GoogleScholarSearchURL = "https://scholar.google.com/scholar?q=";
+        public static string GoogleCiteURL          = "https://scholar.google.com/scholar.bib?q=info:";
+        public static string GoogleCiteURLSuffix    = ":scholar.google.com/&output=cite&scirp=0&hl=zh-CN";
+        public static string CiteUrlPath            = "//a[@class='gs_citi']";
+        public static string TitlePath              = ".//h3[@class='gs_rt']";
+        public static string SourcePath             = ".//h3[@class='gs_rt']/a";
+        public static string AbstractPath           = ".//div[@class='gs_rs']";
+        public static string ProfilePath            = ".//div[@class='gs_a']";
+        public static string CitePath               = ".//a[@onclick]";
+        public static string CiteIdAttrName         = "onclick";
         #endregion
     }
 }
