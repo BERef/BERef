d75c812fa9352625e3b9457a2d4e6735f61b71ff
Blueve <672454911@qq.com>
2016-01-25 18:29:57 +0800

    Add Spliter class

---
+++ GoogleScholarProvider/Spliter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ScholarProviderInterface;
+using HtmlAgilityPack;
+
+namespace GoogleScholarProvider
+{
+    public class Spliter : ISpliter<HtmlDocument, HtmlNode>
+    {
+
+        #region Implement 'ISpliter'
+        public IEnumerable<HtmlNode> Split(HtmlDocument dataSource)
+        {
+            var nodes = dataSource.DocumentNode.SelectNodes(RuleSet.EntryPath);
+
+            foreach (var node in nodes)
+            {
+                yield return node;
+            }
+        }
+        #endregion
+    }
+}
