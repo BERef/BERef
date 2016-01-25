d72e820c1ea797de3a635d97534dc7b887eb3b62
Blueve <672454911@qq.com>
2016-01-25 18:30:17 +0800

    Update rules

--- GoogleScholarProvider/RuleSet.cs
+++ GoogleScholarProvider/RuleSet.cs
@@ -13,6 +13,8 @@ namespace GoogleScholarProvider
         public static string GoogleScholarSearchURL = "https://scholar.google.com/scholar?q=";
         public static string GoogleCiteURL          = "https://scholar.google.com/scholar.bib?q=info:";
         public static string GoogleCiteURLSuffix    = ":scholar.google.com/&output=cite&scirp=0&hl=zh-CN";
+
+        public static string EntryPath              = "//div[@class='gs_ri']";
         public static string CiteUrlPath            = "//a[@class='gs_citi']";
         public static string TitlePath              = ".//h3[@class='gs_rt']";
         public static string SourcePath             = ".//h3[@class='gs_rt']/a";
