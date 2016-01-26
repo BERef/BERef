86d949d3f6ccada5061ca0be82f7cd9c43969215
EchoYang33 <echo_register@163.com>
2016-01-26 22:52:58 +0800

    Add TitleParser

---
+++ BaiduScholarProvider/TitleParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScholarProviderInterface;
+namespace BaiduScholarProvider
+{
+    public class TitleParser : IParser<HtmlNode>
+    {
+        #region Implement 'IParser'
+        public string Parse(HtmlNode item)
+        {
+            return item.SelectSingleNode(RuleSet.TitlePath)?.InnerText;
+        }
+        #endregion
+    }
