4b41f26345f2874d3539da58efebb5c76ec9266b
Blueve <672454911@qq.com>
2016-01-26 14:41:47 +0800

    Change to interface

---
+++ ScholarProviderInterface/ISearchProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScholarProviderInterface
+{
+    public interface ISearchProvider
+    {
+        #region Public Abstract Method
+        IList<BriefEntry> GetResults(string keyword);
+        #endregion
+    }
+}
