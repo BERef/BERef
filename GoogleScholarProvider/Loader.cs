63afe9969ef067a8ac3295ae5c0f00a71d350b3e
Blueve <672454911@qq.com>
2016-01-27 11:58:43 +0800

    Add Loader class

---
+++ GoogleScholarProvider/Loader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ScholarProviderInterface;
+using HtmlAgilityPack;
+
+namespace GoogleScholarProvider
+{
+    public class Loader : ILoader<HtmlDocument>
+    {
+        #region Implement 'ILoader'
+        public HtmlDocument Load(string keyword)
+        {
+            var htmlWeb = new HtmlWeb();
+            return htmlWeb.Load($"{RuleSet.GoogleScholarSearchURL}{keyword}");
+        }
+        #endregion
+    }
+}
