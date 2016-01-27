d7696b365e7705bdd504e2bb59bcf7821891c638
Blueve <672454911@qq.com>
2016-01-27 11:59:27 +0800

    Add GoogleScholarProviderFactory class

---
+++ GoogleScholarProvider/GoogleScholarProviderFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ScholarProviderInterface;
+
+namespace GoogleScholarProvider
+{
+    public class GoogleScholarProviderFactory : IScholarProviderFactory
+    {
+        #region Implement 'IScholarProviderFactory'
+        public ISearchProvider Create()
+        {
+            return new WebSearchProvider(
+                new Loader(),
+                new Spliter(),
+                new BriefBuilder());
+        }
+        #endregion
+
+    }
+}
