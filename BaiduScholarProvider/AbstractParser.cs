69e4574b7617a83c8d03a75c7f12c17144adb6d8
EchoYang33 <echo_register@163.com>
2016-01-27 22:13:32 +0800

    Add AbstractParser

---
+++ BaiduScholarProvider/AbstractParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScholarProviderInterface;
+namespace BaiduScholarProvider
+{
+    public class AbstractParser : IParser<HtmlNode>
+    {
+        public string Parse(HtmlNode item) 
+            => item.SelectSingleNode(RuleSet.AbstractPath)?.InnerText;
+    }
+}
