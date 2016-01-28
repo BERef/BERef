256e04126e40c37bf5ddd7f5df415ce9ae54b575
EchoYang33 <echo_register@163.com>
2016-01-28 14:05:31 +0800

    Rename DataSource to TDataSource

--- ScholarProviderInterface/ILoader.cs
+++ ScholarProviderInterface/ILoader.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace ScholarProviderInterface
 {
-    public interface ILoader<DataSource>
+    public interface ILoader<TDataSource>
     {
-        DataSource Load(string keyword);
+        TDataSource Load(string keyword);
     }
 }
--- ScholarProviderInterface/ISpliter.cs
+++ ScholarProviderInterface/ISpliter.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace ScholarProviderInterface
 {
-    public interface ISpliter<DataSource, T>
+    public interface ISpliter<TDataSource, T>
     {
-        IEnumerable<T> Split(DataSource dataSource);
+        IEnumerable<T> Split(TDataSource dataSource);
     }
 }
