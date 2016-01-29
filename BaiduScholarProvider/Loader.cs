3ebb9c2d7a5153fe7d0435732f7e861555624a1a
EchoYang33 <echo_register@163.com>
2016-01-29 14:35:41 +0800

    Add Loader class

---
+++ BaiduScholarProvider/Loader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScholarProviderInterface;
+
+namespace BaiduScholarProvider
+{
+    public class Loader : ILoader<HtmlDocument>
+    {
+        #region Implement 'ILoader'
+        public HtmlDocument Load(string keyword)
+        {
+            var htmlWeb = new HtmlWeb();
+            return htmlWeb.Load($"{RuleSet.BaiduScholarUrl}/s?wd={keyword}");
+        }
+        #endregion
+    }
+}
