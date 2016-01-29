fd62d44a797a1cd79ff1383842d1b49399e9d798
EchoYang33 <echo_register@163.com>
2016-01-29 14:35:53 +0800

    Add BaiduScholarProviderFactory class

---
+++ BaiduScholarProvider/BaiduScholarProviderFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScholarProviderInterface;
+
+namespace BaiduScholarProvider
+{
+    public class BaiduScholarProviderFactory : IScholarProviderFactory
+    {
+        public ISearchProvider Create()
+        {
+            return new WebSearchProvider(
+                new Loader(),
+                new Spliter(),
+                new BriefBuilder());
+        }
+    }
+}
