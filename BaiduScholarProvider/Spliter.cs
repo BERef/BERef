4fc39c8a38b911b6ddec08996cdb7c3c6bb3f95d
EchoYang33 <echo_register@163.com>
2016-01-29 14:35:32 +0800

    Add Spliter class

---
+++ BaiduScholarProvider/Spliter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScholarProviderInterface;
+
+namespace BaiduScholarProvider
+{
+    public class Spliter : ISpliter<HtmlDocument, HtmlNode>
+    {
+        #region Implement 'ISpliter'
+        public IEnumerable<HtmlNode> Split(HtmlDocument dataSource)
+        {
+            var nodes = dataSource.DocumentNode.SelectNodes(RuleSet.EntryPath);
+            foreach (var node in nodes)
+            {
+                yield return node;
+            }
+        }
+        #endregion
+    }
+}
