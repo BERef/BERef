32c184c16e412c020456a821403d259d8bc14b1e
Blueve <672454911@qq.com>
2016-03-09 21:05:46 +0800

    Add test case

---
+++ GoogleScholarProviderUnitTest/BriefBuilderTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HtmlAgilityPack;
+using GoogleScholarProvider;
+
+namespace GoogleScholarProviderUnitTest
+{
+    [TestClass]
+    public class BriefBuilderTest
+    {
+        [TestMethod]
+        public void TestBuilder()
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.Load(@"TestData/SearchResultPage.data");
+            var spliter = new Spliter();
+            var iter = spliter.Split(htmlDoc).GetEnumerator();
+            iter.MoveNext();
+            var node = iter.Current;
+
+            var builder = new BriefBuilder();
+            var brief = builder.Build(node);
+
+            Assert.AreEqual("Understanding big data: Analytics for enterprise class hadoop and streaming data", brief.Title);
+            Assert.AreEqual("http://dl.acm.org/citation.cfm?id=2132803", brief.Source);
+            Assert.AreEqual("Abstract Big Data represents a new era in data exploration and utilization, and IBM is uniquely positioned to help clients navigate this transformation. This book reveals how IBM is leveraging open source Big Data technology, infused with IBM technologies, to deliver a  ...", brief.Abstract);
+            Assert.AreEqual("P Zikopoulos, C Eaton - 2011 - dl.acm.org", brief.Profile);
+        }
+    }
+}
---
+++ GoogleScholarProviderUnitTest/SpliterTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HtmlAgilityPack;
+using GoogleScholarProvider;
+using System.Collections.Generic;
+
+namespace GoogleScholarProviderUnitTest
+{
+    [TestClass]
+    public class SpliterTest
+    {
+        [TestMethod]
+        public void TestSpliter()
+        {
+            var htmlDoc = new HtmlDocument();
+            // Read Test File
+            htmlDoc.Load(@"TestData/SearchResultPage.data");
+
+            var spliter = new Spliter();
+            var result = spliter.Split(htmlDoc);
+            int count = 0;
+            foreach (var node in result)
+                count++;
+
+            Assert.AreEqual(10, count);
+        }
+    }
+}
