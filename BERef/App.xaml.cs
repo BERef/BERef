8fd612ac8da6ddbfbca7998d0cc337e688357544
Blueve <672454911@qq.com>
2016-05-25 23:31:51 +0800

    Using en-US in DEBUG mode

--- BERef/App.xaml.cs
+++ BERef/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,11 @@ namespace BERef
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+#if DEBUG
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+#endif
+        }
     }
 }
